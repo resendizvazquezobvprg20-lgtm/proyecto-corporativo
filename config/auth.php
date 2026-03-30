3f62fa057ed1f330281a9c81fbacf633c4a63f25
Osmar Yahir <yahiresendiz34@gmail.com>
2026-03-30 12:47:20 -0600

    Updates 10

--- config/auth.php
+++ config/auth.php
@@ -60,10 +60,12 @@ return [
     */
 
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
+    'users' => [
+        'driver' => 'eloquent',
+        // Cambia App\Models\User::class por App\Models\Usuario::class
+        'model' => App\Models\Usuario::class, 
+    ],
+],
 
         // 'users' => [
         //     'driver' => 'database',
